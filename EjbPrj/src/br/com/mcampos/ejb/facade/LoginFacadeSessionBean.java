2f6e923a8af3f02fe8e8450bddabe5fdd835277f
marcelodecampos <marcelodecampos@uol.com.br>
2010-01-15 01:26:06 +0000

    alterar as rotinas de cadastramento do usuário (inicio do sistema). Pretendo usar uma session específica para lidar com as rotinas de login - incluindo aqui, cadastramento de login, envio de email, reenvio de email e/ou senha.

---
+++ EjbPrj/src/br/com/mcampos/ejb/facade/LoginFacadeSessionBean.java
@@ -0,0 +1,18 @@
+package br.com.mcampos.ejb.facade;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless( name = "LoginFacadeSession", mappedName = "CloudSystems-EjbPrj-LoginFacadeSession" )
+@Remote
+@Local
+public class LoginFacadeSessionBean implements LoginFacadeSession, LoginFacadeSessionLocal
+{
+    public LoginFacadeSessionBean()
+    {
+    }
+}
